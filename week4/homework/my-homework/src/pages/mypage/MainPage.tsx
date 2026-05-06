161159c367dc40ffa2b95c6a4beac46ecdd69ec5
haeni82 <2021111929@dgu.ac.kr>
2026-05-07 05:44:34 +0900

    feat: 공통 컴포넌트(infoCard) 구현

--- week4/homework/my-homework/src/pages/mypage/MainPage.tsx
+++ week4/homework/my-homework/src/pages/mypage/MainPage.tsx
@@ -1,6 +1,28 @@
+import InfoCard from "@/components/InfoCard";
+
 // MainPage.tsx
 const MainPage = () => {
-  return <div>전체 페이지</div>;
+  return (
+    <div>
+      전체 페이지
+      <InfoCard
+        items={[
+          { label: "아이디", value: "dlgosl" },
+          { label: "파트", value: "web" },
+        ]}
+      />
+      <br />
+      <InfoCard
+        items={[
+          { label: "이름", value: "이해니" },
+          { label: "아이디", value: "dlgosl" },
+          { label: "이메일", value: "dlgosl@naver.com" },
+          { label: "나이", value: 25 },
+          { label: "파트", value: "web" },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default MainPage;
